e9e488e4a2bbd90d692861d8ab9d80bdd38d9912
Edward Wilson <4864094+ed0906@users.noreply.github.com>
2024-03-11 08:56:12 +0000

    Integrate Sonatype + Publishing plugins for Sonatype Nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     kotlin("jvm") version "1.6.21" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("maven-publish")
+    signing
 }
 
 subprojects {
     apply {
         plugin("org.jetbrains.kotlin.jvm")
-        plugin("maven-publish")
     }
 
     group = "io.camassia"
@@ -27,16 +28,34 @@ subprojects {
             }
         }
     }
+}
 
-    publishing {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/camassia-io/spring-boot-test-dbunit")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
-                }
+signing {
+    val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
+    val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
+
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        println("Signing Key + Passphrase found")
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions.getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    } else {
+        println("Signing Key or Passphrase missing. Artifacts will not be signed.")
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            val ossrhUsername = providers.environmentVariable("OSSRH_USERNAME")
+            val ossrhPassword = providers.environmentVariable("OSSRH_PASSWORD")
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            } else {
+                println("OSSRH credentials not found")
             }
         }
     }
@@ -44,6 +63,6 @@ subprojects {
 
 tasks {
     wrapper {
-        gradleVersion = "7.4.2"
+        gradleVersion = "8.6"
     }
 }
--- spring-boot-test-dbunit/build.gradle.kts
+++ spring-boot-test-dbunit/build.gradle.kts
@@ -2,12 +2,23 @@ plugins {
     id("org.springframework.boot") version "2.5.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("plugin.spring") version "1.6.21"
+
+    id("maven-publish")
+    signing
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
     withSourcesJar()
+    withJavadocJar()
 }
 
 dependencies {
@@ -27,15 +38,68 @@ tasks {
     bootJar {
         enabled = false
     }
+
+//    register<Jar>("sourcesJar") {
+//        archiveClassifier.set("sources")
+//        from(kotlin.sourceSets.main.get().kotlin)
+//    }
+//
+//    register<Jar>("javadocJar") {
+//        group = JavaBasePlugin.DOCUMENTATION_GROUP
+//        description = "Assembles Javadoc JAR"
+//        archiveClassifier.set("javadoc")
+//    }
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/camassia-io/spring-boot-test-dbunit")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
     publications {
         create<MavenPublication>("maven") {
+            groupId = "io.camassia"
             artifactId = "spring-boot-test-dbunit"
             version = System.getenv("GITHUB_VERSION")
             from(components["kotlin"])
             artifact(tasks.kotlinSourcesJar)
+            //artifact(tasks.getByName("sourcesJar"))
+            artifact(tasks.getByName("javadocJar"))
+
+            pom {
+                name.set("spring-boot-test-dbunit")
+                description.set("DB Unit Integration for Spring Boot")
+                url.set("https://github.com/camassia-io/spring-boot-test-dbunit")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                    developers {
+                        developer {
+                            id.set("ed0906")
+                            name.set("Ed Wilson")
+                            organization.set("Camassia")
+                            organizationUrl.set("https://camassia.io")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/camassia-io/spring-boot-test-dbunit")
+                        connection.set("scm:git:git://github.com/camassia-io/spring-boot-test-dbunit.git")
+                        developerConnection.set("scm:git:git://github.com/camassia-io/spring-boot-test-dbunit.git")
+                    }
+                    issueManagement {
+                        url.set("https://github.com/camassia-io/spring-boot-test-dbunit/issues")
+                    }
+                }
+            }
         }
     }
 }
